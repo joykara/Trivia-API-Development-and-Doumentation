9a856f174a76b1157b2de582883b1e804b172b42
Jojo <99082005+joykara@users.noreply.github.com>
2022-09-09 16:34:18 +0300

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,34 +9,23 @@ class Header extends Component {
   render() {
     return (
       <div className='App-header'>
-        <h1
-          onClick={() => {
-            this.navTo('');
-          }}
-        >
+
+        <h1 onClick={() => { this.navTo(''); }}>
           Udacitrivia
         </h1>
-        <h2
-          onClick={() => {
-            this.navTo('');
-          }}
-        >
+
+        <h2 onClick={() => { this.navTo(''); }}>
           List
         </h2>
-        <h2
-          onClick={() => {
-            this.navTo('/add');
-          }}
-        >
+
+        <h2 onClick={() => { this.navTo('/add'); }}>
           Add
         </h2>
-        <h2
-          onClick={() => {
-            this.navTo('/play');
-          }}
-        >
+
+        <h2 onClick={() => { this.navTo('/play'); }}>
           Play
         </h2>
+
       </div>
     );
   }
